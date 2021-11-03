39073b79877bccbfff0c6fdd95b35035b607c246
sydney <ssbright11@gmail.com>
2021-11-03 10:40:23 -0700

    email sender with custom message

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path #allows access to html file
 
 #for gmail, must turn less secured apps ON
 
+html= Template(Path('index.html').read_text()) #read text reads it as string
 email = EmailMessage()
 email['from'] = 'rasta dev'
 email['to'] = 'mindfulinquisitor@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars!'
 
-email.set_content(' I am a Python Master! Give me money!!!!')
+email.set_content(html.substitute(name="TinTin"), 'html') # or {'name': 'TinTin} if there are multiple variables
 
 with smtplib.SMTP(host ='smtp.gmail.com', port=587) as smtp:
   smtp.ehlo() #says hello to server
